b0065f854aec08d654cd1bc48bdf6b7a1bd4cc89
arunkumargovindharaj <46838557+arunkumargovindharaj@users.noreply.github.com>
2019-02-21 15:37:02 +0530

    Create gp11.c

---
+++ gp11.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    char s1[100];
+    int i;
+    scanf("%s",&s1);
+   // for(i=0;s1[i]!='\0';i++)
+    //{
+    if(s1[0]=='s')
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+  // }
+    return 0;
+}
